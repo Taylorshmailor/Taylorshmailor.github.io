9bd281e8f2aae0cc3884a7e08af6647f31bee1fd
Taylorshmailor <taylorshmailor@gmail.com>
2022-12-07 02:11:02 -0600

    langauge options implemented

--- javascript/language.js
+++ javascript/language.js
@@ -1,12 +1,8 @@
-const enbtn = 'en';
-const esbtn = 'es';
-const vnbtn = 'vn';
-function change_text(){
-    if (btn.textContent.toLowerCase().includes(enbtn.toLowerCase())) {
-        btn.textContent = esbtn;
-    } else if (btn.textContent.toLowerCase().includes(esbtn.toLowerCase())) {
-        btn.textContent = vnbtn;
-    } else {
-        btn.textContent = enbtn;
-    }
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+    pageLanguage: 'en',
+    autoDisplay: 'true',
+    includedLanguages:'vi,en,es,fr,ko', 
+    layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
 }
