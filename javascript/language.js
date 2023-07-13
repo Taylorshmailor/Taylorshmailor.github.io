02d63711a457e2abbc9251b4a7fe20b16d8edff5
Taylorshmailor <71964499+Taylorshmailor@users.noreply.github.com>
2023-07-12 23:34:58 -0500

    Delete javascript directory

--- javascript/language.js
+++
@@ -1,8 +0,0 @@
-
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({
-    pageLanguage: 'en',
-    autoDisplay: 'true',
-    includedLanguages:'vi,en,es,fr,ko', 
-    layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
-}
